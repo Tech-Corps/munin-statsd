c5d0dad1f5f4e1485bf5231a6e58fd3b4f8c931a
Brian Staszewski <brian.staszewski@tech-corps.com>
2012-10-03 14:47:48 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 munin-statsd
 ============
+A Perl script to send Munin-node data to a StatD instance. As written, this is designed to be run from cron.
 
-Send Munin-node data to a StatD instance
+Requirements
+============
+
+* Munin::Node::Client from CPAN:
+```
+    cpan Munin::Node::Client
+```
+
+* IO::Socket::INET (If you have Perl, you probably already have this)
+
+Configuration
+=============
+Change `schemabase`, `graphitehost`, and `graphiteport` as appropriate for your environment, and `muninhost` and `muninport` if you're not using this for the local machine.
+
+Installation
+============
+1. Clone the repository
+```
+    cd /usr/local/bin
+    git clone https://github.com/Tech-Corps/munin-statsd.git
+```
+
+2. Make sure munin-graphite.pl is executable
+```
+    chmod +x munin-stats/munin-statsd.pl
+```
+
+3. Create a crontab entry that calls the script
+```
+    * * * * * /usr/local/bin/munin-statsd/munin-statsd.pl
+```
---
+++ munin-statsd.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/env perl -w
+
+#
+# munin-statsd.pl
+# 
+# A Munin-Node to StatsD bridge
+#
+#    Author:: Brian Staszewski (<brian.staszewski@tech-corps.com>)
+# Copyright:: Copyright (c) 2012 Tech-Corps, Inc.
+#   License:: GNU General Public License version 2 or later
+# 
+# This program and entire repository is free software; you can
+# redistribute it and/or modify it under the terms of the GNU 
+# General Public License as published by the Free Software 
+# Foundation; either version 2 of the License, or any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+
+use warnings;
+use strict;
+
+use Munin::Node::Client;
+use IO::Socket::INET;
+
+# Configuration
+my $schemabase		= "node.";
+my $graphitehost	= "graphitehost.company.com";
+my $graphiteport	= 8125;
+my $muninhost		= "localhost";
+my $muninport		= 4949;
+
+
+$|++;
+
+my $node = Munin::Node::Client->connect(host => $muninhost,
+										port => $muninport);
+
+my $version		= $node->version;
+my @hostnames	= $node->nodes;
+my $fqdn		= join(".", reverse(split(/\./, $hostnames[0])));
+my @plugins		= $node->list();
+
+my $statsd_sock	= new IO::Socket::INET (
+	PeerAddr	=> $graphitehost.':'.$graphiteport,
+	Proto		=> 'udp'
+) or die "Error creating socket to statsd!\n";
+
+foreach my $plugin (@plugins) {
+	my %data = $node->fetch($plugin);
+	foreach my $stat (keys %data) {
+		my $packet = $schemabase."$fqdn.$plugin.$stat:".$data{$stat}."|g\n";
+		$statsd_sock->send($packet);
+	}
+}
+
+$statsd_sock->close();
+$node->quit;
